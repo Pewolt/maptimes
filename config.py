9ee4a1055d770c1656f7f7de59bef63a3f89784a
Peter Wolters <pete.wolters@web.de>
2025-01-14 12:05:41 +0100

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ config.read('database/config.ini')
 # Datenbankkonfiguration
 DB_CONFIG = {
     'host': config.get('database', 'host'),
+    'port': config.get('database', 'port'),
     'dbname': config.get('database', 'dbname'),
     'user': config.get('database', 'user'),
     'password': config.get('database', 'password')
